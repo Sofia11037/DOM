13e7af1fbcceac0dc5012692d9aa966f4794b455
Sofia11037 <113290148+Sofia11037@users.noreply.github.com>
2023-09-17 10:16:17 +0200

    Delete DOM directory

--- DOM/apps/app3.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    // console.log('hi');
-    const input = form.querySelector('.string');
-    console.log(input.value);
-    input.value = '';
-});
